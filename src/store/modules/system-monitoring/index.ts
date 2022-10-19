b57c2157130cbc9d92a99e013b39b88cfe80904a
wangyifeng <2665939276@qq.com>
2022-10-19 20:39:05 +0800

    完成访问日志模块的增删改查

--- src/store/modules/system-monitoring/index.ts
+++ src/store/modules/system-monitoring/index.ts
@@ -56,6 +56,22 @@ const mutations: MutationTree<ISystemMonitoring> = {
 const actions: ActionTree<ISystemMonitoring, RootState> = {
   // 获取访问日志列表
   selectAccessList({ commit }, payload: ISelectConditions) {
+    payload.condition["loginLocaltion"] = "";
+    selectAccessList(payload).then(res => {
+      const { code, message, data } = res;
+      if (code == 200) {
+        const { content, totalElements } = data;
+        commit("saveAccessList", content);
+        commit("saveAccessTotal", totalElements);
+      } else {
+        ElMessage({ message, type: 'error' });
+      }
+    });
+  }, 
+  
+  // 删除访问日志列表
+  deleteAccessByIds({ commit }, payload: string) {
+    payload.condition["loginLocaltion"] = "";
     selectAccessList(payload).then(res => {
       const { code, message, data } = res;
       if (code == 200) {
--- src/views/index/pages/system-monitoring/access-log.vue
+++ src/views/index/pages/system-monitoring/access-log.vue
@@ -1,7 +1,58 @@
 <template>
   <div id="access-log">
+    <!-- 查询条件 -->
+    <div class="conditions-container">
+
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <el-form-item label="登录地址">
+            <el-input v-model="selectConditions.condition.loginLocaltion" placeholder="登录地址" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
+          <el-form-item label="用户名称">
+            <el-input v-model="selectConditions.condition.username" placeholder="用户名称" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
+          <el-form-item label="操作系统">
+            <el-input v-model="selectConditions.condition.systemOs" placeholder="操作系统" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
+          <el-form-item label="登录时间">
+            <el-date-picker format="YYYY-MM-DD" v-model="accessTime" type="daterange" range-separator="到"
+              value-format="YYYY-MM-DD hh:mm:ss" @change="datePickerChange" start-placeholder="开始日期"
+              end-placeholder="结束日期" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-button @click="selectAccessList" type="primary">搜索 <el-icon class="el-icon--right">
+            <Search />
+          </el-icon>
+        </el-button>
+        <el-button @click="resetConditions">重置 <el-icon class="el-icon--right">
+            <RefreshRight />
+          </el-icon>
+        </el-button>
+        <el-button type="danger" @click="deleteAccessList" :disabled="selectRows.length==0" plain>删除 <el-icon
+            class="el-icon--right">
+            <Delete />
+          </el-icon>
+        </el-button>
+        <el-button type="success" @click="exportAccessList" :disabled="selectRows.length==0" plain>导出 <el-icon
+            class="el-icon--right">
+            <Download />
+          </el-icon>
+        </el-button>
+      </el-row>
+    </div>
+
     <!-- 表格数据 -->
-    <el-table :data="accessList" stripe style="width: 100%">
+    <el-table :data="accessList" stripe style="width: 100%" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" />
       <el-table-column prop="loginId" label="登录ID" width="180" />
       <el-table-column prop="username" label="用户名" width="180" />
       <el-table-column prop="browser" label="浏览器类型" />
@@ -21,24 +72,35 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, reactive, computed } from 'vue';
+import { defineComponent, onMounted, reactive, computed, ref } from 'vue';
+import { Delete, Download, Search, RefreshRight } from '@element-plus/icons-vue'
 
 import { useStore } from "vuex"
 import { ISelectConditions } from './types';
 
+// import { formatDate } from "@/utils/time"
+
+
 export default defineComponent({
   name: "access-log",
+
+  components: {
+    Delete, Download, Search, RefreshRight
+  },
 
   setup() {
     const store = useStore()
 
+    const accessTime = ref("");
+    let selectRows = ref<any[]>([]);
     const selectConditions = reactive<ISelectConditions>({
       pageSize: 10,
       pageNum: 1,
       condition: {
         username: "",
-        loginLocaltion: "",
-        // loginTime: "",
+        systemOs: "",
+        startTime: "",
+        endTime: "",
       },
     });
 
@@ -52,10 +114,43 @@ export default defineComponent({
     const handleSizeChange = (val: number) => store.dispatch("systemMonitoring/selectAccessList", selectConditions);
     const handleCurrentChange = (val: number) => store.dispatch("systemMonitoring/selectAccessList", selectConditions);
 
+    // 带条件搜索
+    const selectAccessList = () => store.dispatch("systemMonitoring/selectAccessList", selectConditions);
+
+
+    // 处理开始时间和结束时间
+    const datePickerChange = (value: any) => {
+      selectConditions.condition.startTime = value[0];
+      selectConditions.condition.endTime = value[1];
+    }
+
+    // 重置搜索条件
+    const resetConditions = () => {
+      accessTime.value = "";
+      selectConditions.condition = {
+        username: "",
+        systemOs: "",
+        startTime: "",
+        endTime: "",
+      }
+    };
+
+    // 表格行选中触发
+    const handleSelectionChange = (values: any[]) => selectRows.value = values;
+
+    // 删除访问日志
+    const deleteAccessList = () => {
+      const ids = selectRows.value.map(row => row.loginId).join(",");
+      store.dispatch("systemMonitoring/deleteAccessByIds", ids);
+    }
+
+    // 导出访问日志
+    const exportAccessList = () => { }
+
 
     return {
-      accessList, accessTotal, selectConditions,
-      handleSizeChange, handleCurrentChange
+      accessList, accessTotal, selectConditions, accessTime, selectRows,
+      handleSizeChange, handleCurrentChange, selectAccessList, resetConditions, datePickerChange, handleSelectionChange, deleteAccessList, exportAccessList
     }
   }
 
@@ -67,10 +162,15 @@ export default defineComponent({
   width: 100%;
   height: 100%;
 
+  .conditions-container {
+    margin-bottom: 20px;
+  }
+
   .pagination-container {
     margin-top: 30px;
     display: flex;
     justify-content: flex-end;
   }
+
 }
 </style>
